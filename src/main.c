dbfa1ff61108c1f448f444b04a55489e9e83a020
Arsène Rurangiza <a.rurangiza@gmail.com>
2023-01-10 20:19:52 +0100

    forking practice

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Arsene <Arsene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 13:41:01 by arurangi          #+#    #+#             */
-/*   Updated: 2023/01/10 15:46:55 by arurangi         ###   ########.fr       */
+/*   Updated: 2023/01/10 20:19:03 by Arsene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 int	main(void)
 {
-	int id = fork();
-	fork();
-	if (id == 0)
-		info_msg(0, "Hello from \033[32mCHILD\033[0m process");
-	else
-		info_msg(0, "Hello from \033[33mPARENT\033[0m process");
+	int id;
+	
+	id = fork();
+	if (id != 0)
+		id = fork();
+
+	success_msg(0, "Process %d ID", id);
 	return (0);
 }
